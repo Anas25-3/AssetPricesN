887023544ff8ae051182ea60780df166b5a4828b
Anas25-3 <anas26k@gmail.com>
2024-11-28 10:32:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 from binance.exceptions import BinanceAPIException
-
 import requests
 import cbpro
 import time
 from os import system
+import pushover
 
 base_url = "https://api.binance.com/api/v3/ticker/price?symbol="
+# Lägg till dina Pushover credentials här
+user_key = "uppfzzq1yx12a3j2rga1z9r1vai5bh"
+api_token = "a4bwz8nyw6n3u5reqwx4z483bua9de"
+
 
 def get_prices(symbol): 
     price = float(requests.get(base_url + symbol).json()['price'])
     return price
-    """return price['price']"""
+    
+# AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA   
+
+def send_notification(message):
+    """Skickar en notifikation till Pushover"""
+    url = 'https://api.pushover.net:443/1/messages.json'
+    data = {
+        'token': api_token,
+        'user': user_key,
+        'message': message,
+    }
+    requests.post(url, data=data)
+
  
 currency = input("Enter A Currency e.g LTCEUR: ")
 cprice = float(input("Enter A Price: "))
@@ -21,8 +37,10 @@ while True:
     print(f"The current price of {currency} is: {price}")
     if tecken == "Up":
         if price >= cprice:
+            send_notification(f"Price of {currency} has reached {price}!")
             print("Price has been reached")
     else:
         if price <= cprice:
+            send_notification(f"Price of {currency} has dropped to {price}!")
             print("Price has been reached")
     time.sleep(3)
